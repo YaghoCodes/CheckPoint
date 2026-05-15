6120049a32f62d217aaf7dd37b291fdc0a87657b
YaghoCodes <yagho.chinaglia@sptech.school>
2026-05-15 20:27:20 -0300

    adcionando estilização e fazendo o perfil

--- public/assets/scripts/sessao.js
+++ public/assets/scripts/sessao.js
@@ -6,12 +6,18 @@ function validarSessao() {
     var c_usuario = document.getElementById("c_usuario");
     var w_usuario = document.getElementById("w_usuario");
     var profile_avatar = document.getElementById("profile-avatar");
+    var comentario_icone1 = document.getElementById("comentario-icone1");
+    var comentario_icone2 = document.getElementById("comentario-icone2");
+    var comentario_icone3 = document.getElementById("comentario-icone3");
 
     if (email != null && nome != null) {
         n_usuario.innerHTML = nome;
         c_usuario.innerHTML = nome[0].toUpperCase();
         w_usuario.innerHTML = nome + ".";
         profile_avatar.innerHTML = nome[0].toUpperCase();
+        comentario_icone1.innerHTML = nome[0].toUpperCase();
+        comentario_icone2.innerHTML = nome[0].toUpperCase();
+        comentario_icone3.innerHTML = nome[0].toUpperCase();
     } else {
         window.location = "../login.html";
     }
